99b597a84c106076eda6d909ccbd9ad0995430b3
FlyAbove <zahaakiqbal@gmail.com>
2022-12-29 16:56:31 +0500

    Create f strings.py

---
+++ resource/f strings.py
@@ -0,0 +1,12 @@
+# Its a better and easy way to concatenate a string and variable in Python code
+# This method isnt used to concatenate two strings with each other
+# This method doesnt require empty string with a space to create a blank space in ur sentence
+# In this method you need to type f"Text", the variable is enclosed in {} brackets 
+
+name = "Tim"
+print(f"My name is {name}")
+
+age = "27"
+sentence = f"Iam {age} years old"
+
+print(sentence)
