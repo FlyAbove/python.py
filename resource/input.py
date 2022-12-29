62f33944f4253bf395375296859632ad6582a016
FlyAbove <zahaakiqbal@gmail.com>
2022-12-29 17:44:22 +0500

    Folder name is being changed

--- resource/input.py
+++ resource/input.py
@@ -1,5 +1,5 @@
 def get_choices(): # we created a function and then called it on 8th line
-    player_choice = input("Enter a choice = rock paper or scissors:") # this variable is to be inputted by user
+    player_choice = input("Enter a choice = rock paper or scissors: ") # this variable is to be inputted by user
     computer_choice = "paper" # pre defined data as string
     choices = {"player": player_choice, "computer": computer_choice} # using dictionary to store player input data and variable data
 
