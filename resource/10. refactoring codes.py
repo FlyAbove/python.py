04f0f25bca76c075fcae5c70920b5c714813428e
FlyAbove <76478580+FlyAbove@users.noreply.github.com>
2022-12-29 19:50:09 +0500

    Update 10. refactoring codes.py

--- resource/10. refactoring codes.py
+++ resource/10. refactoring codes.py
@@ -1,4 +1,4 @@
 # refactoring is process of restructing code while keeping it's original functionality
-# refactoring code is commonly used to make code more simper and understandable
+# refactoring code is commonly used to make code more simpler and understandable
 # refactoring is basically reconstructing code but not changing it's original function
 
