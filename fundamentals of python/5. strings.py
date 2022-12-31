d667de6b01959dc404fe39283729325bc0fe6686
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 01:01:58 +0500

    Create 5. strings.py

---
+++ fundamentals of python/5. strings.py
@@ -0,0 +1,36 @@
+ # Strings are any character written between " "
+ 
+ # We can concatenate strings using + operator
+
+string = "name"
+string += "second name"
+
+print(string)
+
+# Another way to concatenate strings is written down below
+
+name = "John"
+last_name = "Smith"
+full_name = name + " " + last_name
+
+print(full_name)
+
+# We can create multi line strings using """ """ syntax
+
+multi_string = """ 
+I
+am
+a
+good
+boy
+"""
+
+print(multi_string)
+
+# To convert a string to lower or upper case, we use .lower() and .upper() respectively
+# To convert first letter of each string to capital letter
+
+print("LOWER".lower())
+print("upper".upper())
+print("tITLE cHARACTErs".title())
+
