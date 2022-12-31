1aa631d3cdd26c40473ea128beb11b3a68d3df41
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 21:46:49 +0500

    Create 1. variables.py

---
+++ fundamentals of python/1. variables.py
@@ -0,0 +1,16 @@
+# Variables are used to store data values
+# Variable labels can be assigned to an integer, string or float
+# We can create a new Python variable by assigning a value to label with = operator
+# Variable can contain characters, numbers or underscore character
+# We cannot use !,@,$,%,^,&,*,(),- and + character in a variable label
+# Variable label cannot start with a number 
+# Variable label cannot be a python keywords
+# Python keywords have specific meanings in Python, if, while, import, for etc
+
+# Some valid examples of variables are written down below
+
+name = "your name" # This variable is assigned to a string
+_age_ = 39 # This variable is assigned to a number
+marks = 3.7 # This variable is assigned to a float
+
+
