f04b8a1aad4c34c21794b131eb377348016f367f
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 01:30:25 +0500

    Update 6. escaping characters.py

--- fundamentals of python/6. escaping characters.py
+++ fundamentals of python/6. escaping characters.py
@@ -1,8 +1,27 @@
+# Escape character is a backlash \ followed by character you want to insert
+# To insert characters that are illegal in string we use an escape character
+
+
 # If you need to wrap any character in a string with "" 
 # To do that we use \
 
 name = "name \"wrapped\""
-example = "\'wrapped"
+example = "\' wrapped"
 
 print(name)
 print(example)
+
+# To add backslash in string we use \\
+
+name1 = "backslash\\"
+print(name1)
+
+# To add new line without creating a multi line string we use \n
+
+name2 = "new\nline"
+print(name2)
+
+# To delete last character/space in the string we use \b
+
+name3 = "back \bspace" 
+print(name3)
