e730671576cefecf0f63e2a719d2c5d290047c10
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 23:11:34 +0500

    Update 3. data types.py

---
+++ fundamentals of python/3. data types.py
@@ -0,0 +1,35 @@
+# A variable can store multiple types of data 
+# Python supports following types of data 
+# int, float, complex, list, tuple, range, dict, set, frozenset, bool, bytes, bytearray, memoryview, NoneType
+
+# Some valid examples of different types of data that can be stored in a variable are written down below
+
+a = "Hello" # Data type = string
+b = 1 # Data type = integer
+c = 3.0 # Data type = float
+d = 1j # Data type = complex
+e = ["python", "javascript", "c++"] # Data type = list
+f = ("python", "javascript", "c++") # Data type = tuple
+g = {"python", "javascript", "c++"} # Data type = dictionary
+h = frozenset({"python", "javascript", "c++"}) # Data type = frozenset
+i = True # Data type = boolean
+j = b"Hello" # Data type = bytes
+k = bytearray(1) # Data type = bytearray
+l = memoryview(bytes(5)) # Data type = memoryview
+m = None # Data type = NoneType
+
+# You can get the data type of any object by using type() function
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
