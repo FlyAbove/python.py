3df26a74a9defb06c115e6361bdd023e9aa6b33c
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 23:29:23 +0500

    Update 2. expressions and statements.py

--- fundamentals of python/2. expressions and statements.py
+++ fundamentals of python/2. expressions and statements.py
@@ -1,6 +1,6 @@
 # A combination of operators or operands are called expression
 # An operand is an object that is operated on. On the other hand, an operator is a special symbol that performs the arithmetic or logical computations on the operands
-# Some of Python operators are +,-,x,/,**, etc
+# Some of Python operators are +,-,x,/,**, etc (operators are explained in more detail in the operators.py file)
 # An expression in Python is very different from statements in Python
 # A statement is used for creating variables or for displaying values
 
