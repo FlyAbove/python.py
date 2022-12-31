038e376f3372c27d264ee1a6a30410d7845622ab
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 23:22:45 +0500

    Update 3. data types.py

--- fundamentals of python/3. data types.py
+++ fundamentals of python/3. data types.py
@@ -10,13 +10,14 @@ c = 3.0 # Data type = float
 d = 1j # Data type = complex
 e = ["python", "javascript", "c++"] # Data type = list
 f = ("python", "javascript", "c++") # Data type = tuple
-g = {"python", "javascript", "c++"} # Data type = dictionary
+g = {"python", "javascript", "c++"} # Data type = set
 h = frozenset({"python", "javascript", "c++"}) # Data type = frozenset
 i = True # Data type = boolean
 j = b"Hello" # Data type = bytes
 k = bytearray(1) # Data type = bytearray
 l = memoryview(bytes(5)) # Data type = memoryview
 m = None # Data type = NoneType
+n = {"lang": "python"} # Data type = dictionary
 
 # You can get the data type of any object by using type() function
 
@@ -33,3 +34,20 @@ print(type(j))
 print(type(k))
 print(type(l))
 print(type(m))
+print(type(n))
+
+# We can olso check if any variable is string, list, int, float etc
+
+print(type(a) == str)
+print(type(b) == int)
+print(type(c) == float)
+print(type(d) == complex)
+print(type(e) == list)
+print(type(f) == tuple)
+print(type(g) == set)
+print(type(h) == frozenset)
+print(type(i) == bool)
+print(type(j) == bytes)
+print(type(k) == bytearray)
+print(type(l) == memoryview)
+print(type(n) == dict)
