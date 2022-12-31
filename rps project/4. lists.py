179ccd0cf8ef77268e58a49515925170f125e8fe
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 22:58:35 +0500

    Update 4. lists.py

--- rps project/4. lists.py
+++ rps project/4. lists.py
@@ -1,8 +1,8 @@
-# it allows you to store different values to single variables 
+# Tt allows you to store different values to single variables 
 
 # Important : the difference between dictionary and list is 
-# 1. lists are used as "variable name" = ["value1", "value2"]
-# 2. dictionaries are used to store key value pairs, whereas lists are used to assign single variable with different values
+# 1. Lists are used as "variable name" = ["value1", "value2"]
+# 2. Dictionaries are used to store key value pairs, whereas lists are used to assign single variable with different values
 
 list = ["pizza", "cheese", "cake"]
 
