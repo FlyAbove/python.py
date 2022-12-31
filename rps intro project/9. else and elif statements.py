344901db3518ae1bb532694fa18391d5d4166986
FlyAbove <zahaakiqbal@gmail.com>
2022-12-31 20:48:23 +0500

    Update 9. else and elif statements.py

--- rps intro project/9. else and elif statements.py
+++ rps intro project/9. else and elif statements.py
@@ -1,6 +1,6 @@
 # elif is short form of else if statement
 # elif is used to check if the first statement isnt true, then program checks if the elif statement is true
-# if and Elif statements always ends with :
+# if and elif statements always ends with :
 # else statement is triggered when none of the if or elif statement is true
 
 number = 0 # You can change this number and check how program executes
