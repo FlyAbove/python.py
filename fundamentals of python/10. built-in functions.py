60cc9173f9623bdecec2898756bd04ca3bcb0871
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 17:56:08 +0500

    Update 10. built-in functions.py

---
+++ fundamentals of python/10. built-in functions.py
@@ -0,0 +1,10 @@
+# Absolute and Round off are some basic built in functions supported by Python
+
+# Some valid examples are written down below
+
+numb = -5.5
+absolute = abs(numb)
+roundoff = round(numb)
+
+print(absolute)
+print(roundoff)
