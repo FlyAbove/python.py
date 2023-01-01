c3ef0650e5603cf5be9256f2f84026122f956249
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 17:51:58 +0500

    Update 9. complex numbers.py

---
+++ fundamentals of python/9. complex numbers.py
@@ -0,0 +1,15 @@
+# Complex numbers contains imaginary part (iota) and real part (natural numbers)
+# Iota is denoted as j in Python
+
+# Some valid examples of complex numbers are written down below
+
+complex_number = 2 + 1j # 2 is real part and 1 is imaginary part
+number = complex(2,3) # 2 is real part and 3 is imaginary part
+
+print(complex_number)
+print(number)
+
+# We can print only real or imaginary part as floats from complex numbers by using .real and .imag respectively
+
+print(complex_number.real)
+print(number.imag)
