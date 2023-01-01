59689fcc33fd167c8a40766a31d2bb88cd8bbf90
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 19:13:44 +0500

    Update 12. input.py

---
+++ fundamentals of python/12. input.py
@@ -0,0 +1,7 @@
+# Input function allows user to assign any kind of data to a specific variable
+# input() function is used to collect data from the user
+
+# Some valid examples are written down below
+
+name = input("What is your name? ")
+print(f"My name is {name}")
