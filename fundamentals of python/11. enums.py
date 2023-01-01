f00d3ba8f4060cf93db9ac2d096c43620a983945
FlyAbove <zahaakiqbal@gmail.com>
2023-01-01 18:26:09 +0500

    Create 11. enums.py

---
+++ fundamentals of python/11. enums.py
@@ -0,0 +1,3 @@
+# Enums are readable names bound to a constant value
+# Enumerations in Python are implemented by using the module named "enum".
+# Enumerations are created using classes. Enums have names and values associated with them
