f2e71689ba8e794c6dbfac2405743e4d592e8cfc
FlyAbove <zahaakiqbal@gmail.com>
2023-01-02 22:53:18 +0500

    Update 14. lists.py

---
+++ fundamentals of python/14. lists.py
@@ -0,0 +1,11 @@
+# List is an important Python data structure
+# Lists are used to store multiple value to a single variable 
+# Lists can store different types of data to a single variable
+
+# Some valid examples of lists are written down below
+
+food = ['pizza', 'burger', 'bread', '']
+print(food)
+
+different_data_type_list = ["string", 10, 10.7, 1+1j, food]
+print(different_data_type_list)
