3ce76b1123ecad3f89570e93ab779d8f956cb555
FlyAbove <zahaakiqbal@gmail.com>
2023-01-12 02:03:16 +0500

    Update 16. dictionaries.py

--- fundamentals of python/16. dictionaries.py
+++ fundamentals of python/16. dictionaries.py
@@ -2,12 +2,14 @@
 
 # Some valid examples of dictionaries are written down below
 
-dictionary = { "school": "idk", "name": "John", "grade": "10", 7: "int", ("tuple"): "tuple" }
+dictionary = { "school": "idk", "name": "John", "grade": 10, 7: "int", ("tuple"): "tuple" }
 print(dictionary)
 
 # If you need to print a value of a key we use print(dict_name["key_name"])
 
 print(dictionary["school"])
+print(dictionary.get("grade"))
+
 
 # We can olso modify the value of a key in a dictionary
 
