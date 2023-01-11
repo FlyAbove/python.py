d9a8d61a0b4d522de535768c287d21953f30552f
FlyAbove <zahaakiqbal@gmail.com>
2023-01-12 01:44:11 +0500

    Update 16. dictionaries.py

---
+++ fundamentals of python/16. dictionaries.py
@@ -0,0 +1,8 @@
+# Dictionaries allow you to create key value pairs
+
+# Some valid examples of dictionaries are written down below
+
+dictionary = { "school": "idk", "name": "John", "grade": "10" }
+print(dictionary)
+
+print(len(dictionary))
