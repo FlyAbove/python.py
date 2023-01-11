c05dfe621f2c81fdac071c9af9f9212e1f102af0
FlyAbove <zahaakiqbal@gmail.com>
2023-01-12 01:46:25 +0500

    Update 16. dictionaries.py

--- fundamentals of python/16. dictionaries.py
+++ fundamentals of python/16. dictionaries.py
@@ -2,7 +2,6 @@
 
 # Some valid examples of dictionaries are written down below
 
-dictionary = { "school": "idk", "name": "John", "grade": "10" }
+dictionary = { "school": "idk", "name": "John", "grade": "10", 7: "int", ("tuple"): "tuple" }
 print(dictionary)
 
-print(len(dictionary))
