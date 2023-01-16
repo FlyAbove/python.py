3fbd3b12e927b2234a6a1d6b44af5536a19e8fb1
FlyAbove <zahaakiqbal@gmail.com>
2023-01-16 17:08:51 +0500

    Update 17. sets.py

---
+++ fundamentals of python/17. sets.py
@@ -0,0 +1,19 @@
+# Sets is a collection of data which is unordered, unchangeable and unindexed
+# Set items are unchangeable, but you can remove items and add new items
+
+# Some valid examples of sets are written down below
+
+set1 = {"Home", "Thomas"}
+set2 = {"Home", "Shelby", "Alpha"}
+
+# Sets in maths can be intersected
+# We use & to show intersection ∩ symbol in maths
+
+intersection = set1 & set2 
+print(intersection)
+
+# We can take a union of sets like we take one in maths
+# We use | symbol to show union U symbol in maths
+
+union = set1 | set2 
+print(union)
